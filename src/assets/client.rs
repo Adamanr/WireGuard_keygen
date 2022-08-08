df59e796b9331e50a4c18919707a73d8ca9b15d5
Adamanr <molchanov_03@bk.ru>
2022-08-08 11:26:52 +0500

    Cola version

--- src/assets/client.rs
+++ src/assets/client.rs
@@ -2,68 +2,65 @@ pub mod api{
     use std::{process::Command, str};
     use std::fs::File;
     use std::io::Read;
-    use std::thread::sleep;
-    use std::time::Duration;
-    use std::path::Path;
 
-    pub async fn generate_private_key(){
+    pub async fn generate_private_key(ip:&str) -> String{
+        let file_name = format!("privatekey_{}",ip);
+        let gen_private = format!("wg genkey > {}",file_name);
         Command::new("bash")
             .arg("-c")
-            .arg("wg genkey > privatekey")
-            .spawn().ok().expect("Error");
+            .arg(gen_private.as_str())
+            .spawn().ok().expect("Error generate private_key");
+        file_name
     }
 
-    pub async fn generate_public_key(){
+    pub async fn generate_public_key(ip:&str,private_key:&str){
+        let public_key = format!("publickey_{}",ip);
+        let gen_public = format!("wg pubkey < {} > {}",private_key,public_key);
         Command::new("bash")
             .arg("-c")
-            .arg("wg pubkey < privatekey > publickey")
-            .spawn().ok().expect("Error");
+            .arg(gen_public.as_str())
+            .spawn().ok().expect("Error generate public key");
     }
 
     pub fn add_client(pb_key: &str, ip:&str){
         Command::new("bash")
             .arg("-c")
             .arg(format!("wg set wg0 peer {} {}",pb_key,ip))
-            .spawn().ok().expect("Error");
+            .spawn().ok().expect("Error add client");
     }
 
     pub fn save_client(){
         Command::new("bash")
             .arg("-c")
             .arg("wg-quick save /etc/wireguard/wg0.conf")
-            .spawn().ok().expect("Error");
+            .spawn().ok().expect("Error save client");
     }
 
-    #[allow(unused)]
     pub fn delete_client(pb_key:&str){
         Command::new("bash")
             .arg("-c")
-            .arg("wg set wg0 peer base64_client_publickey remove")
-            .spawn().ok().expect("Error");
+            .arg("wg set wg0 peer")
+            .arg(pb_key)
+            .arg("remove")
+            .spawn().ok().expect("Error remove client");
 
         Command::new("bash")
             .arg("-c")
             .arg("wg-quick save /etc/wireguard/wg0.conf")
-            .spawn().ok().expect("Error");
-
+            .spawn().ok().expect("Error save client after remove");
     }
 
     pub async fn read_key(ip:&str)->(String,String) {
-        generate_public_key().await;
-        generate_private_key().await;
+        let priv_key = generate_private_key(ip).await;
+        generate_public_key(ip,priv_key.as_str()).await;
+
         let mut private_key = String::new();
         let mut public_key = String::new();
 
-        while !Path::new("privatekey").exists() && !Path::new("publickey").exists()  {
-            sleep(Duration::from_millis(800));
-        }
-        sleep(Duration::from_millis(500));
-        let mut file = File::open("privatekey").expect("Error find privatekey file");
+        let mut file = File::open(format!("privatekey_{}",ip).as_str()).expect("Error find privatekey file");
         file.read_to_string(&mut private_key).unwrap();
-
-        let mut file = File::open("publickey").expect("Error find publickey file");
+        let mut file = File::open(format!("publickey_{}",ip).as_str() ).expect("Error find publickey file");
         file.read_to_string(&mut public_key).unwrap();
-
         add_client(public_key.as_str(), ip);
         save_client();
         (private_key.trim().parse().unwrap(), public_key.trim().parse().unwrap())
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use actix_web::{web, App, HttpServer, Result, Responder};
+use std::fs::File;
+use actix_web::{post, get, web, App, HttpServer, Result, Responder};
 use serde::{Deserialize,Serialize};
-use crate::assets::client::api::{ read_key};
+use tokio::fs;
+use crate::assets::client::api::{delete_client, read_key};
 
 mod assets;
 
@@ -15,20 +17,36 @@ struct Key {
     public_key:String
 }
 
-async fn index(info: web::Json<Info>) -> Result<impl Responder> {
+#[post("/add_key")]
+async fn add(info: web::Json<Info> ) -> Result<impl Responder> {
+    File::create(format!("publickey_{}", info.ip).as_str()).expect("Error create file pulibckey");
+    File::create(format!("privatekey_{}", info.ip).as_str()).expect("Error create file privatekey");
     let (pb_key,pt_key) = read_key(info.ip.as_str()).await;
     let keys = Key{
         private_key:pt_key,
         public_key:pb_key
     };
+    fs::remove_file(format!("publickey_{}",info.ip)).await.expect("Can`t remove file");
+    fs::remove_file(format!("privatekey_{}",info.ip)).await.expect("Can`t remove file");
     Ok(web::Json(keys))
+}
+
+#[get("/delete_key")]
+async fn delete(info: web::Json<Info> )-> Result<String>{
+    delete_client(info.ip.as_str());
+    Ok(String::from("Удалено!"))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Keygen run!");
-    HttpServer::new(|| App::new()
-        .route("/getIp", web::post().to(index)))
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("")
+                .service(add)
+                .service(delete)
+        )
+    })
         .bind(("0.0.0.0", 2525))?
         .run()
         .await
